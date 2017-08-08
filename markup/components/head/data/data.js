e9921ad948dddceebe85e6bc95bc2ddd9544ad52
Pavel <shirokuiuna@mail.ru>
2017-08-08 14:16:30 +0300

    upload files

---
+++ markup/components/head/data/data.js
@@ -0,0 +1,14 @@
+head: {
+    defaults: {
+        title: 'Главная страница',
+        useSocialMetaTags: true
+    },
+    "photo": {
+      title: 'Фотографии',
+      useSocialMetaTags: true
+    },
+    "form": {
+      title: 'Конкурс',
+      useSocialMetaTags: true
+    }
+}
