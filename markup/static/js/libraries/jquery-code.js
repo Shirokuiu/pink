0a6ad00cb069dba5eb5d80885ff7407731f2bc06
Pavel <shirokuiuna@mail.ru>
2017-09-11 18:14:11 +0300

    add menu toggle

---
+++ markup/static/js/libraries/jquery-code.js
@@ -0,0 +1,49 @@
+$(function () {
+  $("#reviewsSlider").owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay: false,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsive:{
+      0:{
+        items: 1,
+        smartSpeed: 1000,
+        autoplay: true
+      },
+      600:{
+        items: 1,
+        smartSpeed: 1000,
+        autoplay: true
+      },
+      940:{
+        items: 1,
+        smartSpeed: 1000,
+        autoplay: true
+      },
+      1000:{
+        items: 1,
+        smartSpeed: 10
+      }
+    }
+  });
+  
+  var pageHeaderRowTop = $('#pageHeaderRowTop');
+  var mainNavButton = $('#mainNavButton');
+  var mainNav = $('#mainNav');
+  var mainNavList = $('#mainNavList');
+  
+  mainNavButton.click(function () {
+    pageHeaderRowTop.toggleClass('page-header__rowTop--open');
+    mainNav.toggleClass('main-nav--open');
+    mainNavList.toggleClass('main-nav__list--open');
+  });
+  
+  if (window.matchMedia('(max-width: 610px)').matches) {
+    $('.payment__slider').addClass('owl-carousel');
+    $('#paymentSlider').owlCarousel({
+      startPosition: 1,
+      items: 1
+    });
+  };
+});
